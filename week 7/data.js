cfc06401540df3b0d85a6bfd46304f99a617194e
Vikassuthar78 <vikassuthar6377@gmail.com>
2025-10-07 09:41:28 +0530

    week 7

---
+++ week 7/data.js
@@ -0,0 +1,9 @@
+export const books = [
+    { title: "JavaScript Essentials", author: "John Doe", price: 499, availability: "in stock" },
+    { title: "Learn React", author: "Jane Smith", price: 699, availability: "in stock" },
+    { title: "Node.js in Action", author: "Mark Lee", price: 599, availability: "out of stock" },
+    { title: "Python for Everyone", author: "Ramya Krishna", price: 450, availability: "in stock" },
+    { title: "let's start java", author: "Ramya Krishna", price: 950, availability: "in stock" },
+    { title: "AIML", author: "Ramya Krishna", price: 1050, availability: "in stock" },
+    { title: "JAVA SCRIPT", author: "Ramya Krishna", price: 750, availability: "in stock" }
+];
